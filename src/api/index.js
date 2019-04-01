9562f4b1f803ecc4a0fa0267c2d357848a2abaf2
lidong <642540749@qq.com>
2019-04-01 10:02:47 +0800

    #标大大PC 接口地址

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 
 // const baseURL = 'http://api.biaodaa.com/'
-// const baseURL = 'http://pre.biaodaa.com/'
-const baseURL = '/'
+const baseURL = 'http://pre.biaodaa.com/'
+// const baseURL = '/'
 
 
 
