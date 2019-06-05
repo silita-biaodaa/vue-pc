7e4670de4a03c1863cfd35dca4d63904d72188a4
lidong <642540749@qq.com>
2019-06-05 11:43:41 +0800

    1143

--- src/components/root.vue
+++ src/components/root.vue
@@ -67,7 +67,7 @@ export default {
   },
   methods: {
     gainCode() {
-       if(!(/^1[3|4|5|7|8][0-9]\d{8,11}$/.test(this.iphone))) {
+       if(!(/^((13[0-9])|(15[^4])|(166)|(17[0-8])|(18[0-9])|(19[8-9])|(147,145))\d{8}$/.test(this.iphone))) {
          return this.erip = true
        }
        if(!(this.msg  == '获取验证码') && !(this.msg  == '重新发送')  ) {
