752054bbc3a6e7e8b45507dcde8a7e27dd9b9171
caoxiaoyi123 <285654767@qq.com>
2019-07-01 10:08:12 +0800

    1008

---
+++ src/components/business/punish.vue
@@ -0,0 +1,106 @@
+<!-- 模型： DOM 结构 -->
+<template>
+    <!-- 行政处罚 -->
+    <div class="punish">
+        <div class="ic-basic">
+            <div class="list-nav">
+                <div class="left" style="width:72px" >序号</div>
+                <div class="left" style="width:200px" >决定书文号</div>
+                <div class="left" style="width:100px" >违法行为类型</div>
+                <div class="left" style="width:150px" >行政处罚内容</div>
+                <div class="left" style="width:150px" >决定机关名称</div>
+                <div class="left" style="width:100px" >处罚决定日期</div>
+            </div>
+            <div class="list-co" v-for="(el,i) in list" :key="i" >
+                <div class="left" style="width:72px">{{i+1}}</div>
+                <div class="left" style="width:200px">
+                    <span style="color:#FE6603" >{{el.comName}}</span>
+                </div>
+                <div class="left" style="width:100px">{{el.legalPerson}}</div>
+                <div class="left" style="width:150px" >{{el.phone}}</div>
+                <div class="left" style="width:150px" >{{el.phone}}</div>
+                <div class="left" style="width:100px"></div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'punish', // 基本信息
+    data() {
+        return {
+            // 数据模型
+        }
+    },
+    watch: {
+        // 监控集合
+    },
+    props: {
+        // 集成父级参数
+        list:{
+            
+        }
+    },
+    beforeCreate() {
+        // console.group('创建前状态  ===============》beforeCreate');
+    },
+    created() {
+        // console.group('创建完毕状态===============》created');
+    },
+    beforeMount() {
+        // console.group('挂载前状态  ===============》beforeMount');
+    },
+    mounted() {
+        // console.group('挂载结束状态===============》mounted');
+        this.$nextTick(function() {
+            // console.log('执行完后，执行===============》mounted');
+        });
+    },
+    beforeUpdate() {
+        // console.group('更新前状态  ===============》beforeUpdate');
+    },
+    updated() {
+        // console.group('更新完成状态===============》updated');
+    },
+    beforeDestroy() {
+        // console.group('销毁前状态  ===============》beforeDestroy');
+    },
+    destroyed() {
+        // console.group('销毁完成状态===============》destroyed');
+    },
+    methods: {
+        // 方法 集合
+    }
+
+}
+
+</script>
+<!-- 增加 "scoped" 属性 限制 CSS 属于当前部分 -->
+<style  lang='less' scoped>
+.ic-basic {
+    margin: 0 10px 15px 10px; 
+    border: 1px solid #f2f2f2;
+    box-sizing: border-box;
+    .list-nav {
+        height: 40px;
+        line-height: 40px;
+        text-align: center;
+        font-size: 12px;
+        color:#333;
+        border-bottom: 1px solid #f2f2f2;
+    }
+    .list-co {
+        color: #999;
+        text-align: center;
+        min-height: 40px;
+        display: flex;
+        align-items: center;
+        font-size: 12px;
+        padding: 5px 0;
+        box-sizing: border-box; 
+        border-bottom: 1px solid #f2f2f2
+    }
+
+}
+
+</style>
