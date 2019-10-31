c26c394d2cb5cadedd258d86ec832f5b05bdd816
caoxiaoyi123 <285654767@qq.com>
2019-10-31 14:56:14 +0800

    资质添加问题（少个判断条件）

--- src/components/screenZZ.vue
+++ src/components/screenZZ.vue
@@ -341,18 +341,22 @@ export default {
             }
             // 会员拦截 end
             for(let x of this.lengthList){
-                if(!x.three.code||x.two.code==''){
-                    return this.$confirm('请将上一级资质填充满,再添加下一级资质！', '提示', {
+                if(x.two.code===''){
+                    this.$confirm('请将上一级资质填充满,再添加下一级资质！', '提示', {
                         type: 'warning',
                         showCancelButton: false,
                         showConfirmButton: false
                     })
-                }else if(x.three.list.length!=0&&(!x.three.code||x.three.code=='')){
-                    return this.$confirm('请将上一级资质填充满,再添加下一级资质！', '提示', {
-                        type: 'warning',
-                        showCancelButton: false,
-                        showConfirmButton: false
-                    })
+                    return false
+                }else{
+                    if(x.three.list.length!=0&&x.three.code==''){
+                        this.$confirm('请将上一级资质填充满,再添加下一级资质！', '提示', {
+                            type: 'warning',
+                            showCancelButton: false,
+                            showConfirmButton: false
+                        })
+                        return false
+                    }
                 }
             }
             //不可重复 end
