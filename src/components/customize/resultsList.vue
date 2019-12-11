b6928149a76af08af6170a7727c3dc45c1a2e724
fTaylorswift <1600538919@qq.com>
2019-12-11 10:55:38 +0800

    文件提交；

---
+++ src/components/customize/resultsList.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="resultsList">
+        <div class="resultsList_body">
+            <div class="fs18 color-150">您的查询条件为：</div>
+            <div class="mt10 pt20 pl20 pr20 bg-ffe list">
+                <el-row v-for="(item,index) in resultList" :key="index" class="fs18 pb20" type="flex">
+                    <el-col :span="2.5" class="fw600">{{item.title}}</el-col>
+                    <el-col :span="21">{{item.info}}</el-col>
+                </el-row>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        breadList: {
+            type: Array,
+            default: ""
+        },
+    },
+    data() {
+        return {
+            resultList: [
+                {
+                    title: "选择地区：",
+                    info: "渝内+入渝企业"
+                },
+                {
+                    title: "人员要求：",
+                    info: "注册建造师-二级建造师-建筑工程"
+                },
+                {
+                    title: "资质要求：",
+                    info: "建筑业资质 - 建筑工程施工总承包 - 一级及以上"
+                },
+                {
+                    title: "业绩要求：",
+                    info: "业绩来源 - 全国建筑市场监管公共服务平台,业绩来源 - 全国建筑市场监管公共服务平台业绩来源 - 全国建筑市场监管公共服务平台业绩来源 - 全国建筑市场监管公共服务平台业绩来源 - 全国建筑市场监管公共服务平台,业绩来源 - 全国建筑市场监管公共服务平台业绩来源 - 全国建筑市场监管公共服务平台业绩来源 - 全国建筑市场监管公共服务平台"
+                },
+                {
+                    title: "选择地区：",
+                    info: "渝内+入渝企业"
+                },
+            ]
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+@import "../../style/publicCSS";
+.resulstList {
+    .resultsList_body {
+        // list
+    }
+}
+</style>
---
+++ src/page/customize/result.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="result">
+        <v-head3></v-head3>
+        <div class="result_body bg-fff">
+            <v-publicBread :breadList="breadList"></v-publicBread>
+            <v-result></v-result>
+        </div>
+    </div>
+</template>
+<script>
+import resultsList from "@/components/customize/resultsList";
+import head3 from "@/components/head3";
+import publicBread from "@/components/customize/publicBread";
+export default {
+    components: {
+        "v-result": resultsList,
+        "v-head3": head3,
+        "v-publicBread": publicBread,
+    },
+    data() {
+        return {
+            breadList: [{ title: "查询列表" }], //面包屑列表,以对象形式添加;
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+@import "../../style/publicCSS";
+.result {
+    .result_body{
+        width: 1020px;
+        margin: 0 auto;
+    }
+}
+</style>
