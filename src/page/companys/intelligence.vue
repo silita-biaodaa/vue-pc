f8e52e1d1450480873b98836f898109f0e206a13
lidong <642540749@qq.com>
2019-12-19 16:08:57 +0800

    1608

--- src/page/companys/intelligence.vue
+++ src/page/companys/intelligence.vue
@@ -1,26 +1,26 @@
 <template>
 	<div class="intell" v-loading="loading" element-loading-text="拼命加载中">
 		<div class="in-nav">
-			<span class="left in-po" :class="this.name == '全部' ? 'current' : ''" @click="allshow">全部</span>
-			<div class="left" v-for="(el,i) in allList" :key="i">
-				<span class="in-po" :class=" name == el.qualType ? 'current' : ''" @click="changeList(el)">{{el.qualType}} ({{el.list.length}})</span>
-			</div>
+			<span class="left in-po" :class="this.name == '全部' ? 'current' : ''" @click="allshow">全部 ({{num}})<span class="in-bor" v-if="this.name == '全部'" ></span></span>
+				<span v-for="(el,i) in allList" :key="i" class="left in-po" :class=" name == el.qualType ? 'current' : ''" @click="changeList(el)">{{el.qualType}} ({{el.list.length}}) <span class="in-bor" v-if="name == el.qualType" > </span>  </span>
 		</div>
 		<div class="in-table">
-			<div class="law-nav">
-				<div class="left law-hei" style="width:74px">序号</div>
-				<div class="left" style="width:200px">资质名称</div>
-				<div class="left" style="width:150px">资质证书号</div>
-				<div class="left" style="width:250px">发证机关</div>
-				<div class="left" style="width:100px">有效日期</div>
-			</div>
-			<div class="law-nav" v-for="(el,i) in showArr" :key="i" v-show="!result">
-				<div class="left law-row" style="width:74px">{{i+1}}</div>
-				<div class="left law-row" style="width:200px;lineHeight:15px">{{el.qualName ? el.qualName: '--'}}</div>
-				<div class="left law-row" style="width:150px">{{el.certNo ? el.certNo: '--'}}</div>
-				<div class="left law-row" style="width:250px">{{el.certOrg ? el.certOrg: '--'}}</div>
-				<div class="left law-row" style="width:100px">{{el.validDate ? el.validDate: '--'}}</div>
-			</div>
+				<table class="table-content">
+						<tr>
+							<td style="width:74px">序号</td>
+							<td style="width:200px">资质名称</td>
+							<td style="width:150px">资质证书号</td>
+							<td style="width:calc(100% - 524px)">发证机关</td>
+							<td style="width:100px">有效日期</td>
+						</tr>
+						<tr v-for="(el,i) in showArr" :key="i" v-show="!result">
+							<td>{{i+1}}</td>
+							<td>{{el.qualName ? el.qualName: '--'}}</td>
+							<td>{{el.certNo ? el.certNo: '--'}}</td>
+							<td>{{el.certOrg ? el.certOrg: '--'}}</td>
+							<td>{{el.validDate ? el.validDate: '--'}}</td>
+						</tr>
+				</table>
 			<div class="no-toast" v-show="result">
 				<img src="../../assets/img/bank_card @2x.png" alt="">
 				<span>Sorry，暂未查询到该公司的资质信息</span>
@@ -41,7 +41,8 @@
 				name: '全部',
 				id: '',
 				result: false,
-				loading: true
+				loading: true,
+				num:0
 			}
 		},
 		methods: {
@@ -52,6 +53,7 @@
 					if (res.code == 1) {
 						this.allList = res.data
 						this.allList.forEach((el, i) => {
+							this.num += el.list.length
 							this.allArr.push.apply(this.allArr, el.list)
 						})
 						this.showArr = this.allArr
@@ -91,7 +93,7 @@
 		background-color: #fff;
 		padding-bottom: 50px;
 		.current {
-			color: #333;
+			color: #EB651B;
 		}
 		.el-loading-spinner .path {
 			stroke: #FE6603;
@@ -101,38 +103,28 @@
 		}
 		.in-nav {
 			font-size: 14px;
-			color: #999;
-			height: 44px;
-			line-height: 44px;
-			padding-left: 22px;
+			color: #000;
+			height: 28px;
+			line-height: 28px;
 			font-weight: 550;
+			margin-bottom: 20px;
 			.in-pdd {
 				padding: 0 5px;
 			}
+		
 			.in-po {
 				cursor: pointer;
+				margin-right: 30px;
+				position: relative;
 			}
-		}
-		.in-table {
-			margin: 0 9px;
-			border: 1px solid #f2f2f2;
-			font-size: 12px;
-			color: #333;
-			.law-nav {
-				min-height: 40px;
-				border-bottom: 1px solid #f2f2f2;
-				display: flex;
-				align-items: center;
-				font-size: 12px;
-				color: #333;
-				font-weight: 550;
-				text-align: center;
-				.law-row {
-					min-height: 40px;
-					display: flex;
-					align-items: center;
-					justify-content: center;
-				}
+			.in-bor {
+				position: absolute;
+				bottom: 0;
+				left: 50%;
+				transform: translateX(-50%);
+				width: 30px;
+				height: 2px;
+				background-color: #EB651B;
 			}
 		}
 		.no-search {
